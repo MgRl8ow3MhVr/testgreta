c13977b97664a2c38cdf24881c437014397c5065
PierreM <7anUDQ2A3MVJkC7J@gmail.com>
2021-06-16 11:58:26 +0200

    premier commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+
+  // le state aura deux valeurs possibles :
+  // "option1" dans le cas ou on a clique sur la case du haut
+  // "option2" dans le cas ou on a clique sur la case du bas
+  const [selecteur1, setSelecteur1] = useState("")
+  const [prixOption1, setprixOption1] = useState(0)
+
+
+  // le state aura deux valeurs possibles :
+  // "option1" dans le cas ou on a clique sur la case du haut
+  // "option2" dans le cas ou on a clique sur la case du bas
+
+  const [selecteur2, setSelecteur2] = useState("")
+  const [prixOption2, setprixOption2] = useState(0)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      TESLA CONFIG
+      <div className={selecteur1 === "option1" ? 'selecteurYes' : 'selecteurNo'} onClick={() => {
+        setSelecteur1("option1")
+        setprixOption1(90700)
+      }}
+      >grande autonomie (90 700)</div>
+
+
+      <div className={selecteur1 === "option2" ? 'selecteurYes' : 'selecteurNo'} onClick={() => {
+
+        setSelecteur1("option2")
+        setprixOption1(106700)
+
+      }} > performance (106700)</div>
+
+      COUELUR
+      <div className={selecteur2 === "option1" ? 'selecteurYes' : 'selecteurNo'} onClick={() => {
+        setSelecteur2("option1")
+        setprixOption2(0)
+      }}
+      >Blanc Nacre (0)</div>
+
+
+      <div className={selecteur2 === "option2" ? 'selecteurYes' : 'selecteurNo'} onClick={() => {
+        setSelecteur2("option2")
+        setprixOption2(1000)
+      }} > Noir Uni (1000)</div>
+
+
+      <div>PRIX FINAL {prixOption1 + prixOption2}</div>
+
     </div>
+
   );
 }
 
